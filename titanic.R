553d440d59e7a916a47dc40a2ef97c6342576de3
pnicewicz421 <peter.nicewicz@gmail.com>
2018-05-03 07:32:40 -0400

    Add files via upload

--- titanic.R
+++ titanic.R
@@ -21,6 +21,10 @@ full$Title <- lava::trim(full$Title)
 full$FirstName <- gsub("^.*,.*?\\.[[:space:]]([[:graph:]]*).*$", "\\1", full$Name)
 full$FirstName <- gsub("[\\(|\\)]", "", full$FirstName)
 
+full$OtherName <- ifelse(grepl('^.+\\({1}.+[[:space:]](\\w*)\\){1}$', full$Name), 
+       gsub('^.+\\({1}.+[[:space:]](\\w*)\\){1}$', '\\1', full$Name),
+       "")
+      
 # Titles will be used to estimage age
 # Reduce unusual titles to the four major buckets
 full$Title[full$Title=="Col"] = "Mr"
@@ -39,43 +43,144 @@ full$Title[full$Title=="Rev"] = "Mr"
 full$Title[full$Title=="Sir"] = "Mr"
 full$Title[full$Title=="the Countess"] = "Mrs"
 
+GetMissing <- function(group) {
+  # order from highest to lowest
+  group <- sort(group, decreasing=TRUE)
+  if (length(group) > 1) {
+    output <- group[1] - sum(group[2:length(group)])
+  } else
+    output <- group
+}
+
 # Start with the Group Number as the ticket number. 
 # We will modify this field to join some groups
 # that had separate ticket entries
-full$GroupNumber <- full$Ticket
+#full$GroupNumber <- full$Ticket
 
 # Get Number in Group
-full$NumberInGroup <- full$SibSp + full$Parch + 1
 
-ErrorReport <- function(dframe, groupVar) {
-  dframe %>% 
-  dplyr::group_by(groupVar)
-}
+set.seed(1232313)
 
+full$Ticket <- as.character(full$Ticket)
+
+InGroups <- full %>% group_by_at(vars(LastName, Ticket)) %>%
+  dplyr::mutate(GroupNumber=sample(1:1000000,1))
+
+
+write.csv(InGroups, "output.csv", row.names=FALSE)
+
+
+set.seed(1232313)
+InGroups <- full %>% group_by(Ticket) %>% 
+    dplyr::mutate(GroupNumber=sample(1:1000000,1))
+                  
+                  #NumLastNames = length(unique(LastName)),
+                  #LastNames = ifelse(NumLastNames==1, LastName, paste0(LastName, collapse = ";")))
+
+# ungroup
+
+AdjustGroupNumber <- function(GroupNumber, LastName) {
+  print (GroupNumber)
+  print (LastName)
+  print ("%%%%%%%%%%%")
+  NumGroupNumbers <- length(unique(GroupNumber))
+  if (NumGroupNumbers == 1) {
+    GroupNumber
+  } else {
+    newGroupNumber = sample(1:1000000,1)
+    gn <- unique(GroupNumber)
+    for (each in gn) {
+      print ("$$$$$$$$$$$$$")
+      print(each)
+      print(newGroupNumber)
+      InGroups[which(InGroups$GroupNumber==each),]$GroupNumber <- newGroupNumber
+    }
+    newGroupNumber
+  }
+ }
+
+InGroupsOutput <- InGroups %>% group_by(LastName) %>%
+    mutate(GroupNumber=AdjustGroupNumber(GroupNumber, LastName))
+
+
+#############
+# GET ERROR #
+#############
+
+ErrorTable <- InGroups %>% 
+  dplyr::group_by(GroupNumber) %>%
+  dplyr::mutate(TotalSibSp = sum(SibSp),
+                TotalParch = sum(Parch),
+                MissingSibSp = TotalSibSp %% 2,
+                MissingParch = TotalParch %% 2) %>%
+  dplyr::filter(MissingSibSp == 1 | MissingParch == 1)
+
+write.csv(ErrorTable, "errortable.csv", row.names=FALSE)
+
+
+############
+set.seed(1232313)
 InGroups <- full %>% 
               dplyr::group_by(Ticket) %>%
-              dplyr::mutate(GroupNumber=sample(1:1000,1))
+              dplyr::mutate(GroupNumber=sample(1:1000000,1))
 
-ErrorGroup <- InGroups %>% 
+write.csv(InGroups, "output.csv", row.names=FALSE)
+
+GroupOtherNames <- function(OtherName) {
+  OtherName <- OtherName[which(nchar(OtherName)!=0)]
+  if (length(OtherName) > 1) {
+    paste0(OtherName, collapse=";") 
+  } else if (length(OtherName) == 1) {
+    OtherName
+  } else if (length(OtherName) == 0) {
+    ""
+  }
+}
+
+GroupSummary <- InGroups %>% 
   dplyr::group_by(Ticket) %>%
-  dplyr::summarize(TotalSibSp = sum(SibSp),
+  dplyr::mutate(NumberInGroup = n(),
+                NumberInFamily = Sibsp + Parch + 1,
+                  TotalSibSp = sum(SibSp),
                    TotalParch = sum(Parch),
-                   CountPerTicket = n(),
                    GroupNumber = unique(GroupNumber),
                    LastNames = length(unique(LastName)),
-                   LastName = ifelse(LastNames==1, LastName, paste0(LastName, collapse = ";"))) %>%
-  dplyr::filter(TotalSibSp%%2 != 0 | TotalParch%%2 != 0) 
+                   LastName = ifelse(LastNames==1, LastName, paste0(LastName, collapse = ";")),
+                   OtherName = GroupOtherNames(OtherName),
+                   MissingSibSp = TotalSibSp %% 2,
+                   MissingParch = TotalParch %% 2)
+
+ErrorGroup <- as.list(GroupSummary %>% dplyr::filter(MissingSibSp > 0 | MissingParch > 0) %>% 
+              select(GroupNumber))$GroupNumber
 
 # match these suckers
-# we are looking for families (where lastname is the same) with 
-JoinedFamilies <- ErrorGroup %>% 
-                  dplyr::group_by(LastName) %>%
-                  dplyr::summarize(Ticket=paste0(as.character(Ticket), collapse=";"),
-                                   GroupNumber=max(GroupNumber),
-                                   TotalSibSp=sum(TotalSibSp),
-                                   TotalParch=sum(TotalParch),
-                                   CountPerGroup=sum(CountPerTicket),
-                                   LastNames=sum(LastNames))
+# we are looking for families (where lastname is the same) with
+
+ChooseGroupNumber <- function(GroupNumber) {
+  if (length(unique(GroupNumber)) == 1) {
+    unique(GroupNumber)
+  } else {
+    Err <- GroupNumber[which(GroupNumber %in% ErrorGroup)]
+    if (length(Err)==1) {
+      Err
+    } else if (length(Err) > 1) {
+      max(Err)
+    } else {
+      max(GroupNumber)
+    }
+  }
+}
+
+GroupSummary2 <- InGroups %>% 
+  #dplyr::filter(GroupNumber %in% ErrorGroup)
+  dplyr::group_by(LastName) %>%
+  dplyr::mutate(GroupNumber = ChooseGroupNumber(GroupNumber))
+
+
+
+ErrorGroup2 <- as.list(GroupSummary2 %>% dplyr::filter(MissingSibSp > 0 | MissingParch > 0) %>% 
+                        select(GroupNumber))$GroupNumber
+
 
 JoinedFamilies <- JoinedFamilies %>% mutate(LastName=strsplit(LastName, ";")) %>%
   tidyr::unnest(LastName)
